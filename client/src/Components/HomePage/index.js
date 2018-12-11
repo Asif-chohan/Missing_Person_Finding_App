6a4ce44d7dd28290e8399bd2b697ad9b8dd67fdb
Asif-chohan <rmasifchohan@gmail.com>
2018-12-11 14:35:50 +0500

    carousel ready

--- client/src/Components/HomePage/index.js
+++ client/src/Components/HomePage/index.js
@@ -1,12 +1,43 @@
 import React, { Component } from 'react'
 import Navbar from './Navbar'
+import ImageSlider from '../imagesSlider'
 export default class Home extends Component {
     render() {
         return (
             <div>
                 <Navbar />
+                <ImageSlider slides={slides} />
+               
                 
             </div>
         )
     }
 }
+
+
+const slides = [
+    {
+      city: 'Paris',
+      country: 'France',
+      img: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/142996/paris.jpg',
+    },
+    {
+      city: 'Singapore',
+      img: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/142996/singapore.jpg',
+    },
+    {
+      city: 'Prague',
+      country: 'Czech Republic',
+      img: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/142996/prague.jpg',
+    },
+    {
+      city: 'Amsterdam',
+      country: 'Netherlands',
+      img: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/142996/amsterdam.jpg',
+    },
+    {
+      city: 'Moscow',
+      country: 'Russia',
+      img: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/142996/moscow.jpg',
+    },
+  ];
