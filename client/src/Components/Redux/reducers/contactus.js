244cabe566cd272937d21d86c866cd30f12b93e8
zubairahsan <zubairashan202@gmail.com>
2018-12-12 22:27:21 +0500

    redux is implemented on contact form

--- client/src/Components/Redux/reducers/contactus.js
+++ client/src/Components/Redux/reducers/contactus.js
@@ -2,7 +2,7 @@ const CONTACT_US = 'CONTACT_US';
 
 
 const contactUs = (state = { response: '' }, action) => {
-    alert('rducer')
+    
     let { type, payload } = action;
     switch (type) {
         case CONTACT_US:
