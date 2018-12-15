7c6f1c8bd66c1b6e9f37d29a9698207e4f8ad85c
zubairahsan <zubairashan202@gmail.com>
2018-12-16 00:26:27 +0500

    police form

--- client/src/Components/Navigation/index.js
+++ client/src/Components/Navigation/index.js
@@ -2,8 +2,9 @@ import React, { Component } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Home from '../HomePage';
 import Login from '../login';
-import Signup from '../signup'
-import addPerson from '../AddPerson'
+import Signup from '../signup';
+import addPerson from '../AddPerson';
+import policeForm from '../policeForm';
 
 export default class Navigation extends Component {
   render() {
@@ -15,7 +16,7 @@ export default class Navigation extends Component {
           <Route exact path="/signin" component={Login} /> 
           <Route exact path="/signup" component={Signup} /> 
           <Route exact path="/addPerson" component={addPerson} /> 
-
+          <Route exact path="/policeForm" component={policeForm} />
 
         </div>
       </Router>
